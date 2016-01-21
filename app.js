4f40d3c1604326d78472742726f915f5a80df6b7
Ian Valverde <valverij@gmail.com>
2016-01-21 17:12:27 -0500

    added ingredients import

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-var MongoClient = require('mongodb').MongoClient,
-    config = require('./config.json'); // relies on a config.json file
+var MongoClient = require('mongodb').MongoClient;
+var config = require('./config.json'); // relies on a config.json file
+
+var ingredients = require('./imports/ingredients.json');
     
-function handleError(err) {
-    if (err) {
-        console.log("ERR: ", err.message);
-        return false;
+function handleError(err, db) {
+    if (err) {        
+        try { 
+            db.close(); 
+        } catch (e) {
+            // ignore errors on db close
+        } finally {
+            console.log('ERR: ', err.name, err.message);
+            return false;    
+        }        
     } else {
         return true;
     }
 }    
 
 MongoClient.connect(config.connectionString, function (err, db) {
-    if (handleError(err)) {
-        console.log("Success!");
-        
+    if (handleError(err, db)) {
         db.collection('ingredients', { "name": "Salt" }, function (err, collection) {
-            if (handleError(err)) {
-                console.log("connected");
-                collection.findOne({ "name": "Salt" }, function (err, results) {
-                    console.dir(results);
-                })
+            if (handleError(err, db)) {                
+                collection.insertMany(ingredients, function (err, result) {
+                    if (!err) 
+                        console.log('INSERTED!');
+                    else
+                        console.log('ERR: ', err.name, err.message);
+                    db.close();
+                });                
             }
         }); 
     }
